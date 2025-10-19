e445527c6424eedb4498862509893d24e64c9c11
aboubakr-jelloulat <abubakerjelloulat@gmail.com>
2025-10-19 17:32:32 +0100

    cpp 04

---
+++ ex01/Brain.cpp
@@ -0,0 +1,72 @@
+#include "Brain.h"
+
+#include <iostream>
+
+Brain::Brain() : _idea_count(0)
+{
+	std::cout << "Default constructor of Brain called." << std::endl;
+}
+
+Brain::Brain(const Brain &src)
+{
+	std::cout << "Copy constructor of Brain called." << std::endl;
+
+	this->_idea_count = src._idea_count;
+
+	for (int i = 0; i < _idea_count; i++)
+	{
+		this->ideas[i] = src.ideas[i];
+	}
+}
+
+Brain &Brain::operator=(const Brain &src)
+{
+	std::cout << "Copy assignment operator of Brain called." << std::endl;
+
+	if (this == &src)
+		return (*this);
+
+	_idea_count = src._idea_count;
+
+	for (int i = 0; i < _idea_count; i++)
+	{
+		this->ideas[i] = src.ideas[i];
+	}
+
+	return (*this); 
+}
+
+
+Brain::~Brain()
+{
+	std::cout << "Destructor of Brain called." << std::endl;
+}
+
+void Brain::AddIdea(std::string idea)
+{
+	if (this->_idea_count == MAX)
+	{
+		std::cout << "Your brain is full no more space for ideas" << std::endl;
+
+		return ;
+	}
+
+	ideas[_idea_count] = idea;
+	_idea_count++;
+
+	std::cout << "Added " << _idea_count << " new idea"  << (_idea_count > 1 ? "s." : ".") << std::endl;
+}
+
+std::string &Brain::GetIdeaByIndex(int index)
+{
+	/*
+		static means this variable is created once and lives for the entire program lifetime
+			not just inside the function call
+	*/
+	static std::string NotFound = "404 Not Found";
+
+    if (index < 0 || index >= 100)
+        return NotFound;
+
+    return ideas[index];
+}
---
+++ ex01/Cat.cpp
@@ -0,0 +1,54 @@
+#include "Cat.h"
+
+#include <iostream>
+
+Cat::Cat() : brain(new Brain())
+{
+	std::cout << "Default constructor of Cat called." << std::endl;
+
+	this->type = "Cat";
+}
+
+Cat::Cat(const Cat &src)   : Animal(src)
+{
+	std::cout << "Copy constructor of Cat called." << std::endl;
+
+	type = src.type;
+
+	brain = new Brain(*src.brain);
+}
+
+Cat &Cat::operator=(const Cat &src)
+{
+	std::cout << "Copy assignment operator of Cat called." << std::endl;
+
+	if (this == &src)
+		return (*this);
+
+	this->type = src.type;
+	*brain = *src.brain;
+
+	return (*this);
+}
+
+
+Cat::~Cat()
+{
+	std::cout << "Destructor of Cat called." << std::endl;
+	delete brain;
+}
+
+void Cat::makeSound() const
+{
+	std::cout << "Meow!" << std::endl;
+}
+
+void Cat::AddIdea(std::string idea)
+{
+	brain->AddIdea(idea);
+}
+
+std::string &Cat::GetIdeaByIndex(int index)
+{
+	return brain->GetIdeaByIndex(index);
+}
